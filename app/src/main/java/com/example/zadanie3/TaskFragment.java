03bc5d3ff3251baed83f614642d5a51166fc05eb
Adam Grzeszczuk <103129308+Geerafix@users.noreply.github.com>
2023-11-09 13:12:09 +0100

    commit

---
+++ app/src/main/java/com/example/zadanie3/TaskFragment.java
@@ -0,0 +1,77 @@
+package com.example.zadanie3;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.UUID;
+
+// to jest klasa wyświetlająca jedno zadanie
+public class TaskFragment extends Fragment {
+    private Task task;
+    private EditText nameField;
+    private Button dateButton;
+    private CheckBox doneCheckBox;
+    public static final String TASK_ID = "task_id";
+
+    public TaskFragment() {}
+
+    public static TaskFragment newInstance(UUID taskId) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(TASK_ID, taskId);
+        TaskFragment taskFragment = new TaskFragment();
+        taskFragment.setArguments(bundle);
+        return taskFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        UUID taskId = (UUID)getArguments().getSerializable(TASK_ID);
+        task = TaskStorage.getInstance().getTask(taskId);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+
+        nameField = view.findViewById(R.id.task_name);
+        dateButton = view.findViewById(R.id.task_date);
+        doneCheckBox = view.findViewById(R.id.task_done);
+
+        nameField.setText(task.getName());
+        nameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                task.setName(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+
+        dateButton.setText(task.getDate().toString());
+        dateButton.setEnabled(false);
+
+        doneCheckBox.setChecked(task.isDone());
+        doneCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            task.setDone(isChecked);
+        });
+        return view;
+    }
+}
